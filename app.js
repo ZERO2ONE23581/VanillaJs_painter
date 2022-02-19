a97fde1d8cc7d556f1f444cdf789a7a2e22458d6
aiden <zero2one23581@gmail.com>
2022-02-19 22:57:17 +0900

    context

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,6 +10,13 @@ function onMouseMove(event) {
   //offset show coordinate of X-axis, Y-axis
   const x = event.offsetX;
   const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 function onMouseDown(event) {
   painting = true;
@@ -13,12 +24,13 @@ function onMouseDown(event) {
 function stopPainting() {
   painting = false;
 }
-function onMouseUp(event) {
-  stopPainting();
+function startPainting() {
+  painting = true;
 }
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove); //캔버스에 마우스 움직일때
-  canvas.addEventListener("mousedown", onMouseDown); // 마우스 잡고있을때
-  canvas.addEventListener("mouseup", onMouseUp); // 마우스 놓았을때
-  canvas.addEventListener("mouseleave", onMouseUp); //마우스 나갔을때
+  canvas.addEventListener("mousedown", startPainting); // 마우스 잡고있을때
+  canvas.addEventListener("mouseup", stopPainting); // 마우스 놓았을때
+  canvas.addEventListener("mouseleave", stopPainting); //마우스 나갔을때
 }
